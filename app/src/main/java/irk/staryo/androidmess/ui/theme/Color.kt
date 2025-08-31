14a7857a5d98768b2a19441514f2defdcba41ba3
Staryo40 <arrrryow@gmail.com>
2025-08-31 07:33:43 +0700

    init: initialization

---
+++ app/src/main/java/irk/staryo/androidmess/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package irk.staryo.androidmess.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Blue80 = Color(0xFFB3E5FC)     // Light Blue
+val BlueGrey80 = Color(0xFFB0BEC5) // Light Blue-Grey
+val Cyan80 = Color(0xFF80DEEA)     // Light Cyan
+
+val Blue40 = Color(0xFF0288D1)     // Darker Blue
+val BlueGrey40 = Color(0xFF455A64) // Dark Blue-Grey
+val Cyan40 = Color(0xFF00838F)     // Dark Cyan
